f0282cdbae8102780a39f3f01d2348633f4c872f
Messier104ngcc <messier104ngc4594@gmail.com>
2024-11-27 23:50:44 -0300

    Adicionar arquivos de projeto.

---
+++ WebPage/Controllers/ProjetosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebPage.Controllers
+{
+    public class ProjetosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
